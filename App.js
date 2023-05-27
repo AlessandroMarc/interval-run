526844fdfa6d0d11233460d6f36c88e071ce0e19
AlessandroMarc <52158784+AlessandroMarc@users.noreply.github.com>
2023-05-27 13:51:48 +0200

    First commit

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import * as React from "react";
 import { Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigationState } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createStackNavigator } from "@react-navigation/stack";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import HomeScreen from "./components/HomeScreen";
 import SettingsScreen from "./components/SettingScreen";
@@ -10,33 +9,43 @@ import LoginScreen from "./components/LoginScreen";
 
 const Tab = createBottomTabNavigator();
 
-export default function App() {
+const user = false;
+
+function MainScreen() {
+    const navigationState = useNavigationState((state) => state);
+    const isLoginScreenSelected = navigationState.index === 0 && navigationState.routes.length === 1;
+
     return (
         <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
+            {isLoginScreenSelected ? (
+                <LoginScreen />
+            ) : (
+                <Tab.Navigator
+                    screenOptions={({ route }) => ({
+                        tabBarIcon: ({ focused, color, size }) => {
+                            let iconName;
 
-                        if (route.name === "Home") {
-                            iconName = focused ? "ios-information-circle" : "ios-information-circle-outline";
-                        } else if (route.name === "Settings") {
-                            iconName = focused ? "ios-list" : "ios-list-outline";
-                        } else if (route.name === "Login") {
-                            iconName = focused ? "ios-list" : "ios-list-outline";
-                        }
+                            if (route.name === "Home") {
+                                iconName = focused ? "ios-information-circle" : "ios-information-circle-outline";
+                            } else if (route.name === "Settings") {
+                                iconName = focused ? "ios-list" : "ios-list-outline";
+                            }
 
-                        // You can return any component that you like here!
-                        return <Ionicons name={iconName} size={size} color={color} />;
-                    },
-                    tabBarActiveTintColor: "tomato",
-                    tabBarInactiveTintColor: "gray",
-                })}
-            >
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
-                <Tab.Screen name="Login" component={LoginScreen} />
-            </Tab.Navigator>
+                            // You can return any component that you like here!
+                            return <Ionicons name={iconName} size={size} color={color} />;
+                        },
+                        tabBarActiveTintColor: "tomato",
+                        tabBarInactiveTintColor: "gray",
+                    })}
+                >
+                    <>
+                        <Tab.Screen name="Home" component={HomeScreen} />
+                        <Tab.Screen name="Settings" component={SettingsScreen} />
+                    </>
+                </Tab.Navigator>
+            )}
         </NavigationContainer>
     );
 }
+
+export default MainScreen;
